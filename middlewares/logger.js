755ea53584dedb70eb8678472b2d5effc06a7eb1
Miracle Mark <miraclemark147@gmail.com>
2024-11-22 11:51:07 -0800

    working on netlify deploy for all routes -4

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,21 +1,34 @@
-// logger.js
+// // logger.js
+// import { createLogger, format, transports } from 'winston';
+// const { combine, timestamp, printf } = format;
+
+// // Custom log format
+// const customFormat = printf(({ level, message, timestamp }) => {
+//   return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+// });
+
+// // Create the logger
+// const logger = createLogger({
+//   level: 'info', // You can set log levels (error, warn, info, http, verbose, debug, silly)
+//   format: combine(timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), customFormat),
+//   transports: [
+//     // Log to console
+//     new transports.Console(),
+//     // Log to a file
+//     new transports.File({ filename: 'logs/app.log' }),
+//   ],
+// });
+
+// export default logger;
+
+// for deployment
 import { createLogger, format, transports } from 'winston';
-const { combine, timestamp, printf } = format;
 
-// Custom log format
-const customFormat = printf(({ level, message, timestamp }) => {
-  return `${timestamp} [${level.toUpperCase()}]: ${message}`;
-});
-
-// Create the logger
 const logger = createLogger({
-  level: 'info', // You can set log levels (error, warn, info, http, verbose, debug, silly)
-  format: combine(timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), customFormat),
+  level: 'info',
+  format: format.combine(format.timestamp(), format.json()),
   transports: [
-    // Log to console
-    new transports.Console(),
-    // Log to a file
-    new transports.File({ filename: 'logs/app.log' }),
+    new transports.Console(), // Log to the console
   ],
 });
 
