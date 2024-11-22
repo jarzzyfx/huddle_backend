ff30ef8e928b49b1811c7eef25de8ae94e4ee37c
Miracle Mark <miraclemark147@gmail.com>
2024-11-22 11:47:41 -0800

    working on netlify deploy for all routes -3

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 
 // Logging middleware
-app.use((req, res, next) => {
-  logger.info(`${req.method} ${req.url}`);
-  next();
-});
+// app.use((req, res, next) => {
+//   logger.info(`${req.method} ${req.url}`);
+//   next();
+// });
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: true })); // For form submissions
