2721ccc0cd2ffb16d2b1da2d16c99f002ee85a3f
Miracle Mark <miraclemark147@gmail.com>
2024-11-22 11:37:43 -0800

    working on netlify deploy for all routes

--- functions/api.js
+++ functions/api.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import ServerlessHttp from 'serverless-http';
+import TaskRouter from '../routes/tasks.js';
+import UserRouter from '../routes/user.js';
+import WorkroomRouter from '../routes/workroom.js';
 
 const app = express();
 
@@ -8,7 +11,12 @@ app.get('/.netlify/functions/api', (req, res) => {
     message: 'hello world',
   });
 });
-
+app.use('/api/v1/tasks', TaskRouter);
+app.use('/api/v1/user', UserRouter);
+app.use('/api/v1/workroom', WorkroomRouter);
+app.get('/test', (req, res) => {
+  res.send('hello boy!');
+});
 const handler = ServerlessHttp(app);
 
 module.exports.handler = async (event, context) => {
