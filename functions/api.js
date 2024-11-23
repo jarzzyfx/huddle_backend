9ce0cc1feda54e0908f746419951309beb4dea00
Miracle Mark <miraclemark147@gmail.com>
2024-11-23 07:28:44 -0800

    staging for render

--- functions/api.js
+++ functions/api.js
@@ -4,6 +4,8 @@ import connectToDatabase from '../lib/connectDb.js'; // Import MongoDB connectio
 import TaskRouter from '../routes/tasks.js';
 import UserRouter from '../routes/user.js';
 import WorkroomRouter from '../routes/workroom.js';
+import cors from 'cors';
+import { corsOptions } from '../lib/constants.js';
 
 const app = express();
 const router = express.Router();
@@ -28,11 +30,17 @@ router.get('/test', (req, res) => {
 });
 
 app.use(express.json());
+// cors middleware
+app.use(cors(corsOptions));
+
+console.log('TaskRouter:', TaskRouter.default);
+console.log('UserRouter:', UserRouter.default);
+console.log('WorkroomRouter:', WorkroomRouter.default);
 
 // Route definitions
-router.use('/v1/tasks', TaskRouter);
-router.use('/v1/user', UserRouter);
-router.use('/v1/workroom', WorkroomRouter);
+router.use('/v1/tasks', TaskRouter.default);
+router.use('/v1/user', UserRouter.default);
+router.use('/v1/workroom', WorkroomRouter.default);
 
 app.use('/.netlify/functions/api', router);
 
